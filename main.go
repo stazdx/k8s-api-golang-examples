2b2b87edf26da8fb9690ccda04f3eae05d844ba8
Staz Dx <stanislav.93.18@gmail.com>
2022-07-03 22:58:49 -0500

    adding slack integration

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"path/filepath"
+	"strconv"
+	"time"
 
 	// v1 "k8s.io/api/apps/v1"
+	"github.com/slack-go/slack"
 	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
@@ -15,6 +19,8 @@ import (
 
 func main() {
 	fmt.Println("--Testing--")
+
+	webhookSlack()
 
 	var kubeconfig *string
 	kubeconfig = flag.String("kubeconfig", filepath.Join("/Users/stazdx", ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -51,7 +57,7 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(list2.Items)
+	fmt.Println(list2.Items[0])
 
 	// list, err := clientset.AppsV1().Deployments(apiv1.NamespaceDefault).List(ctx, metav1.ListOptions{})
 	// if err != nil {
@@ -60,4 +66,27 @@ func main() {
 
 	// fmt.Println(list.Items)
 
+}
+
+func webhookSlack() {
+	attachment := slack.Attachment{
+		Color:         "good",
+		Fallback:      "You successfully posted by Incoming Webhook URL!",
+		AuthorName:    "Staz Dx",
+		AuthorSubname: "github.com",
+		AuthorLink:    "https://github.com/stazdx",
+		AuthorIcon:    "https://avatars2.githubusercontent.com/u/1691541",
+		Text:          "<!channel> All text in Slack uses the same system of escaping: chat messages, direct messages, file comments, etc. :smile:\nSee <https://api.slack.com/docs/message-formatting#linking_to_channels_and_users>",
+		Footer:        "slack api",
+		FooterIcon:    "https://platform.slack-edge.com/img/default_application_icon.png",
+		Ts:            json.Number(strconv.FormatInt(time.Now().Unix(), 10)),
+	}
+	msg := slack.WebhookMessage{
+		Attachments: []slack.Attachment{attachment},
+	}
+
+	err := slack.PostWebhook("https://hooks.slack.com/services/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", &msg)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
